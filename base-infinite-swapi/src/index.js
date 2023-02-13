35141e09c22913a81bb01a12e8accd10bb517259
quidama <quidama@naver.com>
2023-02-13 21:23:17 +0900

    setup infinite swapi

--- base-infinite-swapi/src/index.js
+++ base-infinite-swapi/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
